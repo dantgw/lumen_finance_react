3d1cf155685e15fce48ca00fa814bd8c8270fbe2
eldante <dan.tgw@gmail.com>
2024-08-19 02:42:09 +0800

    Able to complete the entire loan cycle.

---
+++ src/components/web3/ClaimLoanContractInteraction.tsx
@@ -0,0 +1,124 @@
+import { Button, Card, FormControl, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Stack, VStack } from '@chakra-ui/react'
+import { type FC, useState, useEffect, useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+import 'twin.macro'
+
+import { useSorobanReact } from "@soroban-react/core"
+import * as StellarSdk from '@stellar/stellar-sdk';
+
+import React from 'react'
+import Link from 'next/link'
+
+import { contractInvoke, TxResponse, useRegisteredContract } from '@soroban-react/contracts'
+import { nativeToScVal, ScInt, xdr } from '@stellar/stellar-sdk'
+
+export const ClaimLoanContractInteraction: FC = () => {
+  const sorobanContext = useSorobanReact()
+
+
+  const [, setFetchIsLoading] = useState<boolean>(false)
+  const [updateIsLoading, setUpdateIsLoading] = useState<boolean>(false)
+  const { register, handleSubmit } = useForm()
+  
+  const [updateFrontend, toggleUpdate] = useState<boolean>(true)
+  const [contractAddressStored, setContractAddressStored] = useState<string>()
+
+  // Retrieve the deployed contract object from contract Registry
+  const contract = useRegisteredContract("lumen_finance")
+
+  
+
+  const [invoiceNo, setInvoiceNo] = useState<number>(0);
+  const handleInvoiceNoChange = (valueString: string) => {
+    setInvoiceNo(Number(valueString)); // Convert the string value to a number
+  };
+
+
+  const { activeChain, server, address } = sorobanContext
+
+  const claimLoan = async () => {
+    if (!address) {
+      console.log("Address is not defined")
+      // toast.error('Wallet is not connected. Try again...')
+      return
+    }
+    else if (!server) {
+      console.log("Server is not setup")
+      toast.error('Server is not defined. Unabled to connect to the blockchain')
+      return
+    }
+    else {
+      const currentChain = activeChain?.name?.toLocaleLowerCase()
+      if (!currentChain) {
+        console.log("No active chain")
+        toast.error('Wallet not connected. Try again…')
+        return
+      }
+      else {
+
+        setUpdateIsLoading(true)
+        console.log('invoice_no',invoiceNo)
+        try {
+          const result = await contract?.invoke({
+            method: 'claim_loan',
+            args: [
+              nativeToScVal(invoiceNo, {type: "u32"}),
+            ],
+            signAndSend: true
+          })
+          console.log('🚀 « result:', result);
+          let result2 = result as TxResponse
+          if (result2.status == "SUCCESS") {
+            toast.success("Loan Approval Success!")
+          }
+          else {
+            toast.error("Loan Approval unsuccessful...")
+            
+          }
+        } catch (e) {
+          console.error(e)
+          toast.error('Error while sending tx. Try again…')
+        } finally {
+          setUpdateIsLoading(false)
+          toggleUpdate(!updateFrontend)
+        } 
+
+        // await sorobanContext.connect();
+      }
+    }
+  }
+
+  return (
+
+        <div tw={"rounded-2xl bg-white p-6 flex flex-col space-y-4 shadow-sm max-w-96 w-full"}>
+        <h2 tw="text-center text-2xl font-semibold text-black">Claim Loan</h2>
+          
+          <form onSubmit={handleSubmit(claimLoan)}>
+            <VStack spacing={2} align="middle">
+              <FormControl>
+                  <FormLabel>Invoice Number</FormLabel>
+                  <NumberInput value={invoiceNo} onChange={handleInvoiceNoChange}>
+                  <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
+                
+              <Button
+                type="submit"
+                mt={4}
+                colorScheme="purple"
+                isDisabled={updateIsLoading}
+                isLoading={updateIsLoading}
+              >
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </div>
+
+  )
+}
---
+++ src/pages/view_loan/index.tsx
@@ -0,0 +1,59 @@
+import { HomePageTitle } from '@/components/home/HomePageTitle'
+import { CenterBody } from '@/components/layout/CenterBody'
+import { ChainInfo } from '@/components/web3/ChainInfo'
+import { ConnectButton } from '@/components/web3/ConnectButton'
+import { GreeterContractInteractions } from '@/components/web3/GreeterContractInteractions'
+import { LumenContractInteractions } from '@/components/web3/LumenContractInteractions'
+import type { NextPage } from 'next'
+import 'twin.macro'
+import { Inter } from "next/font/google";
+import { TokenContractInteractions } from '@/components/web3/TokenContractInteractions'
+import { type FC, useState, useEffect, useCallback } from 'react'
+import toast from 'react-hot-toast'
+import { FiChevronDown } from 'react-icons/fi'
+
+import { contractInvoke, useRegisteredContract } from '@soroban-react/contracts'
+import { Address, nativeToScVal, ScInt, xdr } from '@stellar/stellar-sdk'
+
+import { useSorobanReact } from "@soroban-react/core"
+import * as StellarSdk from '@stellar/stellar-sdk';
+import { LoansDashboard } from '@/components/web3/LoansDashboard'
+import { Button, Menu, MenuButton, MenuItem, MenuList, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, useDisclosure } from '@chakra-ui/react'
+import Link from 'next/link'
+import { RequestLoanContractInteractions } from '@/components/web3/RequestLoanContractInteractions'
+import { NavBar } from '@/components/layout/NavBar'
+import { Whitelist } from '@/components/web3/Whitelist'
+import { ApproveLoanContractInteraction } from '@/components/web3/ApproveLoanContractInteraction'
+import { ViewLoansContractInteraction } from '@/components/web3/ViewLoansContractInteraction'
+
+
+const ViewLoanPage: NextPage = () => {
+
+  const sorobanContext = useSorobanReact()
+
+  const { activeChain, server, address } = sorobanContext
+
+  const [, setFetchIsLoading] = useState<boolean>(false)
+  const [updateIsLoading, setUpdateIsLoading] = useState<boolean>(false)
+  
+  
+  const [fetchedBalance, setBalance] = useState<string>()
+  const [updateFrontend, toggleUpdate] = useState<boolean>(true)
+  const [contractAddressStored, setContractAddressStored] = useState<string>()
+  const tokenContract = useRegisteredContract("lumen_usdc")
+
+  return (
+    <main
+      tw={"flex min-h-screen flex-col items-center w-full"}
+    >
+     <NavBar/>
+      <div tw={"min-h-screen w-full pt-20 flex flex-col space-y-12 items-center"}>
+        <ViewLoansContractInteraction/>
+      </div>
+    
+    </main>
+    
+  )
+}
+
+export default ViewLoanPage
